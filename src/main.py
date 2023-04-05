e5136268d38034e862c7a91e6bdf5ddd04fe0a5d
Renko <renkosostaric@gmail.com>
2023-04-05 18:11:56 +0200

    Added support for file list and writing to Excel

--- src/main.py
+++ src/main.py
@@ -1,34 +1,43 @@
 from bs4 import BeautifulSoup
 import re
 import pandas as pd;
+import openpyxl as pyxl;
+from openpyxl.utils.dataframe import dataframe_to_rows
+from pathlib import Path
 
-searchList=["MACHINE","NC PROGRAM PATH", "MATERIAL ID", "BLANK", "WEIGHT", "MACHINING TIME", "LASER TOTAL CUTTING LENGTH", "NUMBER OF PROGRAMME RUNS", "SCRAP"]
-dataList=[""] * 9
-
-""" df = pd.DataFrame({
-    "MACHINE" : [],
-    "NC PROGRAM PATH" : [],
-    "MATERIAL ID" : [],
-    "BLANK" : [],
-    "WEIGHT" : [],
-    "MACHINING TIME" : [],
-    "LASER TOTAL CUTTING LENGTH" : [],
-    "NUMBER OF PROGRAMME RUNS" : [],
-    "SCRAP" : []}); """
-    
 df = pd.DataFrame(columns=["MACHINE","NC PROGRAM PATH", "MATERIAL ID", "BLANK", "WEIGHT", "MACHINING TIME", "LASER TOTAL CUTTING LENGTH", "NUMBER OF PROGRAMME RUNS", "SCRAP"])
+setupPlanList = ["9500.html", "81011.html", "319930.html", "500585.html", "3712951.html", "test.html"]
+setupPlanDirectory = Path("testing/html")
+excelFileDirectory = Path("testing/xlsx")
 
-with open("77581.html") as fp:
-    setupDocument = BeautifulSoup(fp, "html.parser")
-    
-row=[] 
-for column in df:
-    searchElement = setupDocument.find("td", string=re.compile(column))
-    foundElement = searchElement.find_next_sibling("td")
-    clean = foundElement.text
-    cell = " ".join(re.findall(r"\S+(?<!\s)", clean))
-    row.append(cell)
-dfTemp = pd.DataFrame([row])
-dfTemp.columns = df.columns
-df = pd.concat([df, dfTemp], axis=0, ignore_index=True) 
+
+def readFiles(setupPlanDirectory, setupPlanList):
+    for file in setupPlanList:
+        with open(setupPlanDirectory / file) as fp:
+            setupPlan = BeautifulSoup(fp, "html.parser")
+        dataframeAppendFile(setupPlan)
+
+def dataframeAppendFile(setupPlan):
+    global df
+    tempRow=[] 
+    for column in df:
+        searchElement = setupPlan.find("td", string=re.compile(column))
+        foundElement = searchElement.find_next_sibling("td")
+        innerText = foundElement.text
+        formattedText = " ".join(re.findall(r"\S+(?<!\s)", innerText))
+        tempRow.append(formattedText)
+    dfTemp = pd.DataFrame([tempRow])
+    dfTemp.columns = df.columns
+    df = pd.concat([df, dfTemp], ignore_index=True)
+
+def writeToXlsx(excelFileDirectory):
+    workbook = pyxl.load_workbook(excelFileDirectory / "main.xlsx")
+    worksheet = workbook["podatki"];
+    for row in dataframe_to_rows(df, header = True, index = False):
+        worksheet.append(row)
+    workbook.save(excelFileDirectory / "main_output.xlsx")
+    workbook.close()
+
+readFiles(setupPlanDirectory, setupPlanList)
+writeToXlsx(excelFileDirectory)
 print(df)
